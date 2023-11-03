685e33f9447c5d5ce573dd77221ef6aba7d12eb6
Yunus <canitezyunus@gmail.com>
2023-11-03 15:02:26 +0300

    End of Section 1
    Created Project and requirements. Created dbContext, UserController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DatingDbContext _datingDbContext;
+
+    public UsersController(DatingDbContext datingDbContext)
+    {
+        _datingDbContext = datingDbContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _datingDbContext.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await _datingDbContext.Users.FindAsync(id);
+        return user;
+    }
+}
---
+++ API/Data/DatingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DatingDbContext : DbContext
+{
+    public DatingDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
